ca627548908c2e5e0f413a47c0ce5a4fc4f90309
Brandon Tarrant <brandontarrant04@gmail.com>
2024-03-25 00:36:10 -0400

    Create function to fetch data from the server

--- client/script.js
+++ client/script.js
@@ -75,6 +75,20 @@ const handleSubmit = async (e) => {
   const messageDiv = document.getElementById(uniqueId);
 
   loader(messageDiv);
+
+  // fetch data from server => bot's response
+  const response = await fetch("http://localhost:5000", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt: data.get("prompt"),
+    }),
+  });
+
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = "";
 };
 
 form.addEventListener("submit", handleSubmit);
