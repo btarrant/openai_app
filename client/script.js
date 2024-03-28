4070dcb7180eaaf83f5fa0c2901e830851ed9cd7
Brandon Tarrant <brandontarrant04@gmail.com>
2024-03-28 17:24:09 -0400

    Redirect fetch data to come from deployed web service

--- client/script.js
+++ client/script.js
@@ -78,7 +78,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from server => bot's response
   try {
-    const response = await fetch("http://localhost:5000", {
+    const response = await fetch("https://codecraft-wdtq.onrender.com", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
